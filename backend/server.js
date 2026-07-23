63e6a2cf6a99b2d22e442980743200ce5c16ed1e
Nitin Shukla <nitin.shukla12004@gmail.com>
2026-07-23 04:36:09 +0000

    added backend configuration

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.get("/api/message", (req, res) => {
+  res.json({
+    message: "Hello from AWS Backend!"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
